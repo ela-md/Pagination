8c4e41719c3d03b1d9db9378645e122e813e156b
ela.md <ela.ahmadi06@gmail.com>
2025-05-12 13:03:23 -0400

    props added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,24 @@ function App() {
    <div className="App">
 
    <ReactPaginate
-   
+
+        pageCount={50}
+        breakLabel="..."
+        nextLabel="next >"
+        previousLabel='< previous'
+        containerClassName='pagination justify-content-center'
+        pageClassName='page-item'
+        nextClassName='page-item'
+        previousClassName='page-item'
+
+        pageLinkClassName='page-link'
+        nextLinkClassName='page-link'
+        previousLinkClassName='page-link'
+
+        breakClassName='page-item'
+        breakLinkClassName='page-link'
+        activeLinkClassName='active'
+
    
    />
       
