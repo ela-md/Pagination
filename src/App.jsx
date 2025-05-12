2dce9a75977a4e20fbfa1f9f46fe619ef665d6c9
ela.md <ela.ahmadi06@gmail.com>
2025-05-12 12:56:10 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ReactPaginate from 'react-paginate';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+function App() {
+ 
+
+  return (
+   <div className="App">
+
+   <ReactPaginate
+   
+   
+   />
+      
+   </div>
+  )
+}
+
+export default App
